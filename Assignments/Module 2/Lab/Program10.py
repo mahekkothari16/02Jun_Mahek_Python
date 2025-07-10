987d90ac353fac5bdb498c5d6fa9aa48f6079781
mahekkothari16 <mkotharim@gmail.com>
2025-07-10 19:16:50 +0530

    Second Module

--- Assignments/Module 2/Lab/Program10.py
+++ Assignments/Module 2/Lab/Program10.py
@@ -1,1 +1,4 @@
 #Program 10 : Write a Python program to access alternate values between index 1 and 5 in a tuple. 
+tuple = ("Gujarat","Rajasthan","Goa","Kashmir","Banglore","Diu")
+print(tuple[1:5:2])
+
